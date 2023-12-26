fa550d69a53def3065a3a36eb8ad8008186d7170
Soman-Paracha <129701440+Soman-Paracha@users.noreply.github.com>
2023-12-26 21:50:42 +0500

    updated

--- Pages/Article.js
+++ Pages/Article.js
@@ -1,12 +1,95 @@
-import { View, Text } from "react-native"
+// Import necessary components from React Native
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
-const Article=()=>{
-    return(
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                I am Article
-            </Text>
-        </View>
-    )
-}
+
+// Define a generic Card component for articles
+const Card = ({ title, content, imageSource }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={imageSource} style={styles.cardImage} resizeMode="cover" />
+      <View style={styles.cardContent}>
+        <Text style={styles.title}>{title}</Text>
+        <Text>{content}</Text>
+      </View>
+    </View>
+  );
+};
+
+// Define individual articles with sample images
+const Article1 = () => {
+    const title = 'Understanding Diabetes';
+    const content = 'Diabetes is a chronic condition that affects the way your body regulates blood sugar. Learn about its types, symptoms, and management.';
+    const imageSource = require('../Images/3.png');
+    return <Card title={title} content={content} imageSource={imageSource} />;
+};
+
+const Article2 = () => {
+  const title = 'Balanced Diet for Diabetics';
+  const content = 'Discover the importance of a balanced diet for individuals with diabetes. Tips on meal planning and making healthier food choices.';
+  const imageSource = require('../Images/2.png');
+  return <Card title={title} content={content} imageSource={imageSource} />;
+};
+
+const Article3 = () => {
+  const title = 'Exercise and Diabetes';
+  const content = 'Explore the benefits of regular exercise in managing diabetes. Find suitable exercises and create a fitness routine for better health.';
+  const imageSource = require('../Images/3.png');
+  return <Card title={title} content={content} imageSource={imageSource} />;
+};
+
+const Article4 = () => {
+  const title = 'Monitoring Blood Sugar Levels';
+  const content = 'Learn about the importance of monitoring blood sugar levels and the various methods available to keep track of your glucose levels.';
+  const imageSource = require('../Images/2.png');
+  return <Card title={title} content={content} imageSource={imageSource} />;
+};
+
+const Article5 = () => {
+  const title = 'Diabetes and Mental Health';
+  const content = 'Understand the connection between diabetes and mental health. Tips for managing stress and maintaining overall well-being.';
+  const imageSource = require('../Images/3.png');
+  return <Card title={title} content={content} imageSource={imageSource} />;
+};
+
+// Create a component that displays a list of articles in a ScrollView
+const Article = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.articlesContainer}>
+      <Article1 />
+      <Article2 />
+      <Article3 />
+      <Article4 />
+      <Article5 />
+    </ScrollView>
+  );
+};
+
+// Styles for the components
+const styles = StyleSheet.create({
+  articlesContainer: {
+    padding: 16,
+  },
+  card: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    marginBottom: 16,
+    overflow: 'hidden',
+  },
+  cardImage: {
+    width: 100,
+    height: '100%',
+  },
+  cardContent: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
+
 export default Article;
--- Pages/Support.js
+++ Pages/Support.js
@@ -1,10 +1,79 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import React from 'react';
 
-const Support =()=>{
-    return(
-        <View>
-            <Text>I am Support</Text>
-        </View>
-    )
+const Support = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.header}>Diabetes Support</Text>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionHeader}>1. Support Groups</Text>
+        <Text>
+          Join local or online support groups where individuals with diabetes
+          share experiences, tips, and encouragement. Connecting with others
+          facing similar challenges can be empowering.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionHeader}>2. Educational Resources</Text>
+        <Text>
+          Access reliable sources for diabetes education. Learn about
+          self-management, treatment options, and the latest advancements in
+          diabetes care. Knowledge is a powerful tool in managing diabetes.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionHeader}>3. Counseling Services</Text>
+        <Text>
+          Consider seeking professional counseling to address the emotional
+          aspects of living with diabetes. Mental health is an essential part
+          of overall well-being.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionHeader}>4. Lifestyle Coaching</Text>
+        <Text>
+          Work with a lifestyle coach to develop personalized strategies for
+          maintaining a healthy lifestyle. This includes guidance on nutrition,
+          exercise, and stress management.
+        </Text>
+      </View>
+
+      <View style={styles.section}>
+        <Text style={styles.sectionHeader}>5. Online Forums</Text>
+        <Text>
+          Explore reputable online forums where individuals share insights,
+          ask questions, and receive support. However, always verify the
+          reliability of information.
+        </Text>
+      </View>
+
+      {/* Add more sections as needed */}
+
+    </ScrollView>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  section: {
+    marginBottom: 16,
+  },
+  sectionHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
+
 export default Support;
